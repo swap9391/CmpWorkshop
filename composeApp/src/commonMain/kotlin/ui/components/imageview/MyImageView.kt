e8b88675ab869b20034bee20a49806560a543fd4
Swapnil Jadhav <swapnil.jadhav@kohler.com>
2025-02-12 23:21:42 +0530

    Added default image to MyImageView and added TestCompose preview function
    The diffs introduce two changes:
    1.  **MyImageView Default Image**: The `MyImageView` composable now has a default image resource (`Res.drawable.image_slider_one`) if no image resource is provided.

--- composeApp/src/commonMain/kotlin/ui/components/imageview/MyImageView.kt
+++ composeApp/src/commonMain/kotlin/ui/components/imageview/MyImageView.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import cmpworkshop.composeapp.generated.resources.Res
+import cmpworkshop.composeapp.generated.resources.image_slider_one
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun MyImageView(modifier: Modifier = Modifier, imageRes:DrawableResource) {
+fun MyImageView(modifier: Modifier = Modifier, imageRes:DrawableResource=Res.drawable.image_slider_one) {
     Box(modifier) {
         Image(
             painter = painterResource(resource = imageRes),
